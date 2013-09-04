4dcf2c435a0d8f43b42df6488072a28cd323d579
sumit <sumitp.scs@gmail.com>
2013-09-04 18:03:22 +0530

    chnages to master

--- Test/src/CallFromScala.java
+++ Test/src/CallFromScala.java
@@ -1,7 +1,7 @@
 public class CallFromScala {
 
 public void method(){
-	System.out.println("will delete after commit from repo1");
+	System.out.println("will delete after commit from eclipse ");
+	System.out.println("need to merge in test branch");
 }
 }
-	
