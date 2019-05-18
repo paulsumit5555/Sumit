26784db54998654a8fbedba7ebfa47315a4dab4b
Sumit Paul <paulsumit555@gmail.com>
2019-05-18 20:12:15 +0530

    added comment

--- Test/src/CallFromScala.java
+++ Test/src/CallFromScala.java
@@ -1,6 +1,6 @@
 public class CallFromScala {
 
 public void method(){
-	System.out.println("in master for testing gdfgdf");
+	System.out.println("in master for testing gdfgdf from testbranch");
 }
 }
